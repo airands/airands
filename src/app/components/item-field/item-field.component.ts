cdfdecf2db6efcd4a76a616213c7fffc5b94eeb3
Levi Murray <levidavidmurray@gmail.com>
2020-04-28 00:43:13 -0700

    + Improved order list input + Improved OrderItem component

--- src/app/components/item-field/item-field.component.ts
+++ src/app/components/item-field/item-field.component.ts
@@ -1,16 +1,21 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {OrderItem} from "../../interfaces/order";
+import {IonInput} from "@ionic/angular";
 
 @Component({
     selector: 'app-item-field',
     templateUrl: './item-field.component.html',
     styleUrls: ['./item-field.component.scss'],
 })
-export class ItemFieldComponent implements OnInit {
+export class ItemFieldComponent implements AfterViewInit, OnInit {
+
+    @ViewChild('ionInput', {static: false}) ionInput: IonInput;
 
     private _active: boolean;
 
     @Input() item: OrderItem;
+    @Input() skipFocus: boolean;
+    @Input() placeholder: string;
 
     get active(): boolean {
         return this._active;
@@ -21,12 +26,13 @@ export class ItemFieldComponent implements OnInit {
         this._active = active;
     }
 
-    // item: OrderItem = {
-    //     name: '',
-    //     quantity: 1,
-    // };
-
     ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        this.ionInput.getInputElement().then((input) => {
+            setTimeout(() => !this.skipFocus ? input.focus() : null, 50);
+        });
     }
 
 }
