a139326751049c09608fc6899d8567746cd9a9a3
Levi Murray <levidavidmurray@gmail.com>
2020-05-31 13:38:52 -0700

    Styled order summary page Removed unused variables file

--- client/src/app/components/modals/order-workflow-modal/components/order-summary/order-summary.component.ts
+++ client/src/app/components/modals/order-workflow-modal/components/order-summary/order-summary.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NewOrderStore} from "../../../../../store/orders/new-order.store";
-import {OrdersService} from "../../../../../../open_api";
+import {LocationAddress, NewOrder, OrdersService} from "../../../../../../open_api";
+import {AlertController, LoadingController, ModalController, ToastController} from "@ionic/angular";
 
 @Component({
     selector: 'app-order-summary',
@@ -12,20 +13,72 @@ export class OrderSummaryComponent implements OnInit {
     constructor(
         private newOrderStore: NewOrderStore,
         private ordersService: OrdersService,
+        private alertController: AlertController,
+        private loadingController: LoadingController,
+        private toastController: ToastController,
+        private modalController: ModalController,
     ) {
     }
 
+    order: NewOrder;
+
     ngOnInit() {
+        this.order = this.newOrderStore.newOrder;
     }
 
-    goNext() {
-        this.createOrder();
+    async placeOrder() {
+        const loader = await this.loadingController.create({
+            message: 'Placing your order',
+        });
+        await loader.present();
+
+        try {
+            const order = await this.ordersService.createOrder(this.newOrderStore.newOrder).toPromise();
+            await loader.dismiss();
+            const toast = await this.toastController.create({
+                message: "Your order has been placed!",
+                duration: 3000,
+            });
+            await toast.present();
+            await this.modalController.dismiss();
+            console.log('Order Created!', order);
+        } catch (error) {
+            await loader.dismiss();
+            const alert = await this.alertController.create({
+                header: 'An Error Occurred',
+                subHeader: JSON.stringify(error),
+                buttons: ['Dismiss'],
+            });
+            await alert.present();
+        }
     }
 
-    private async createOrder() {
-        console.log('Creating order!', this.newOrderStore.newOrder);
-        const order = await this.ordersService.createOrder(this.newOrderStore.newOrder).toPromise();
-        console.log('Order Created!', order);
+    async saveOrder() {
+        const alert = await this.alertController.create({
+            header: 'TODO',
+            buttons: ['Dismiss'],
+        });
+        await alert.present();
+    }
+
+    get streetAddress(): string {
+        const {street_number, street_name, unit_number} = this.pickUp;
+        const street = `${street_number} ${street_name}`;
+        return unit_number ? `${unit_number}-${street}` : street;
+    }
+
+    get fullAddress(): string {
+        const {streetAddress} = this;
+        const {city, province, postal_code} = this.pickUp;
+        return `${streetAddress}\n${city}, ${province} ${postal_code}`;
+    }
+
+    get pickUp(): LocationAddress {
+        return this.order.locations.pick_up;
+    }
+
+    get dropOff(): LocationAddress {
+        return this.order.locations.drop_off;
     }
 
 }
